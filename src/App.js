9beb8c04bda08d97929fc58f9146ecfa272b5848
vaidehi2401 <vaidehidhole312@gmail.com>
2025-06-27 20:24:32 +0530

    Added Cart

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Cart from './Cart/Cart';
 import Header from "./Layout/Header";
 import RestaurantSummary from './Meals/RestaurantSummary';
 import Meals from './Meals/Meals';
 function App() {
   return (
     <div className='main-div'>
+      <Cart/>
       <Header/>
      <main>
       <Meals/>
